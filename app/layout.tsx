21ec7cbc2346c203c4395353d20ddda3a2d53cb6
Rishad Bharucha <rb2102@gmail.com>
2024-10-09 15:02:38 -0400

    fixed footer height

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,43 +24,44 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang="en" className={GeistSans.className} suppressHydrationWarning>
-			<body className="bg-background text-foreground">
+			<body className="bg-background text-foreground flex flex-col min-h-screen">
 				<ThemeProvider
 					attribute="class"
 					defaultTheme="system"
 					enableSystem
 					disableTransitionOnChange
 				>
-					<main className="min-h-screen flex flex-col items-center">
-						<div className="flex-1 w-full flex flex-col gap-20 items-center">
-							<nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
-								<div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
-									<div className="flex gap-5 items-center font-semibold">
-										<Link href={"/"}>AC G2 Reviews Chatbot</Link>
-									</div>
-									{!hasEnvVars ? <EnvVarWarning /> : <HeaderAuth />}
+					<header className="w-full border-b border-b-foreground/10">
+						<nav className="w-full flex justify-center h-16">
+							<div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
+								<div className="flex gap-5 items-center font-semibold">
+									<Link href={"/"}>AC G2 Reviews Chatbot</Link>
 								</div>
-							</nav>
-							<div className="flex flex-col gap-20 max-w-5xl p-5">
-								{children}
+								{!hasEnvVars ? <EnvVarWarning /> : <HeaderAuth />}
 							</div>
-
-							<footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
-								<p>
-									Powered by{" "}
-									<a
-										href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
-										target="_blank"
-										className="font-bold hover:underline"
-										rel="noreferrer"
-									>
-										Supabase
-									</a>
-								</p>
-								<ThemeSwitcher />
-							</footer>
+						</nav>
+					</header>
+					<main className="flex-grow w-full flex flex-col items-center">
+						<div className="w-full max-w-5xl flex flex-col gap-20 items-center p-5 py-16">
+							{children}
 						</div>
 					</main>
+					<footer className="w-full border-t">
+						<div className="w-full max-w-5xl mx-auto flex items-center justify-center gap-8 py-4 text-center text-xs">
+							<p>
+								Powered by{" "}
+								<a
+									href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
+									target="_blank"
+									className="font-bold hover:underline"
+									rel="noreferrer"
+								>
+									Supabase
+								</a>
+							</p>
+							<ThemeSwitcher />
+						</div>
+					</footer>
 				</ThemeProvider>
 			</body>
 		</html>
