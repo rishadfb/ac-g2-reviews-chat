a9a2cf856ef87f06ccb91a168e9ffee4395d4bb3
Rishad Bharucha <rb2102@gmail.com>
2024-10-09 14:49:21 -0400

    add ac logo

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,20 +1,15 @@
-import NextLogo from "./next-logo";
-import SupabaseLogo from "./supabase-logo";
+import ActiveCampaignLogo from "./ac-logo";
 
 export default function Header() {
 	return (
 		<div className="flex flex-col gap-16 items-center">
 			<div className="flex gap-8 justify-center items-center">
 				<a
-					href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
+					href="https://www.activecampaign.com/"
 					target="_blank"
 					rel="noreferrer"
 				>
-					<SupabaseLogo />
-				</a>
-				<span className="border-l rotate-45 h-6" />
-				<a href="https://nextjs.org/" target="_blank" rel="noreferrer">
-					<NextLogo />
+					<ActiveCampaignLogo />
 				</a>
 			</div>
 			<h1 className="sr-only">Supabase and Next.js Starter Template</h1>
