a0f84446cc316c1a79fe79c585497353e45a900e
Rishad Bharucha <rb2102@gmail.com>
2024-10-09 18:32:18 -0400

    check if authenticated in hero

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,13 +1,29 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
+import { createClient } from "@/utils/supabase/client";
 import { useTheme } from "next-themes";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 import ActiveCampaignLogoDark from "./ac-logo-dark";
 import ActiveCampaignLogoLight from "./ac-logo-light";
 
 export default function Header() {
 	const { resolvedTheme } = useTheme();
+	const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+	useEffect(() => {
+		const supabase = createClient();
+
+		const checkAuth = async () => {
+			const {
+				data: { user },
+			} = await supabase.auth.getUser();
+			setIsAuthenticated(!!user);
+		};
+
+		checkAuth();
+	}, []);
 
 	return (
 		<div className="flex flex-col items-center">
@@ -42,9 +58,15 @@ export default function Header() {
 				.
 			</p>
 			<div className="w-full p-[1px] bg-gradient-to-r from-transparent via-foreground/10 to-transparent mb-8" />
-			<Link href="/chat" passHref>
-				<Button size="lg">Start Chatting</Button>
-			</Link>
+			{isAuthenticated ? (
+				<Link href="/chat" passHref>
+					<Button size="lg">Start Chatting</Button>
+				</Link>
+			) : (
+				<Link href="/sign-in" passHref>
+					<Button size="lg">Sign in</Button>
+				</Link>
+			)}
 		</div>
 	);
 }
