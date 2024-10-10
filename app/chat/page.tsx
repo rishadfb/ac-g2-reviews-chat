de8b6e1122aa0d27c12977bc0ea0e430e2906810
Rishad Bharucha <rb2102@gmail.com>
2024-10-09 21:23:31 -0400

    make reviews chat a client component

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -1,10 +1,5 @@
-"use client";
-
 import { ReviewsChat } from "@/components/chat/reviews-chat";
-import { useChat } from "ai/react";
 
-export default function ChatPage() {
-	const chatProps = useChat();
-
-	return <ReviewsChat {...chatProps} />;
+export default async function ChatPage() {
+	return <ReviewsChat />;
 }
--- components/chat/reviews-chat.tsx
+++ components/chat/reviews-chat.tsx
@@ -1,21 +1,31 @@
-import type { UseChatHelpers } from "ai/react";
+"use client";
+
+import { useChat } from "ai/react";
 import { ChatIntro } from "./chat-intro";
 import { ChatMessages } from "./chat-messages";
 
-export function ReviewsChat(props: UseChatHelpers) {
-	const hasMessages = props.messages.length > 0;
+export function ReviewsChat() {
+	const {
+		input,
+		handleInputChange,
+		handleSubmit,
+		isLoading,
+		setInput,
+		messages,
+	} = useChat();
+	const hasMessages = messages.length > 0;
 
 	return (
 		<div className="container mx-auto p-4 max-w-3xl">
 			{hasMessages ? (
-				<ChatMessages messages={props.messages} />
+				<ChatMessages messages={messages} />
 			) : (
 				<ChatIntro
-					input={props.input}
-					handleInputChange={props.handleInputChange}
-					handleSubmit={props.handleSubmit}
-					isLoading={props.isLoading}
-					setInput={props.setInput}
+					input={input}
+					handleInputChange={handleInputChange}
+					handleSubmit={handleSubmit}
+					isLoading={isLoading}
+					setInput={setInput}
 				/>
 			)}
 		</div>
